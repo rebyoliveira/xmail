a50fab630186796f030b5b052935cdd37e91db8d
rebyoliveira <rebecca.d.oliveira@gmail.com>
2018-12-14 13:44:12 +0200

    Updates

---
+++ precache-manifest.cf201ed95ffb7b18794b03476679fdf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "10ea7e5a60a5240425e0ab46809ce65c",
+    "url": "/rebyoliveira/xmail/static/media/dolphin.10ea7e5a.jpg"
+  },
+  {
+    "revision": "b1cccf3d14e8995f1d7776ac8990af13",
+    "url": "/rebyoliveira/xmail/static/media/avatar.b1cccf3d.jpg"
+  },
+  {
+    "revision": "07d71a8a0ee36b7102aa",
+    "url": "/rebyoliveira/xmail/static/js/runtime~main.07d71a8a.js"
+  },
+  {
+    "revision": "05f61ffa3048ac707824",
+    "url": "/rebyoliveira/xmail/static/js/main.05f61ffa.chunk.js"
+  },
+  {
+    "revision": "935be95d9685ceedd6ca",
+    "url": "/rebyoliveira/xmail/static/js/1.935be95d.chunk.js"
+  },
+  {
+    "revision": "05f61ffa3048ac707824",
+    "url": "/rebyoliveira/xmail/static/css/main.1c3caaea.chunk.css"
+  },
+  {
+    "revision": "599484721057fab6c20702c4a95142db",
+    "url": "/rebyoliveira/xmail/index.html"
+  }
+];
